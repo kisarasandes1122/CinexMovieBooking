1527b4c6d60a838dd4105be4f714c707334ea4fb
dsathyajith <meeriyagallad@gmail.com>
2025-01-13 18:43:10 +0530

    css update

---
+++ src/components/Jayshowtimes/Breadcrumb.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Breadcrumb.css';
+
+const Showtime = () => {
+  return (
+    <div className="breadcrumbmain">
+      <div className="breadcrumb">
+        <span>Home</span>
+        <span className="separator">&gt;</span>
+        <span>Buy Ticket</span>
+      </div>
+    </div>
+  
+  );
+};
+
+export default Showtime;
