e6fbca020ab6b96204e23c80444625e4f71baba0
avish madushanka <avishmadushanka21@gmail.com>
2025-01-13 22:44:00 +0530

    update

---
+++ src/Pages/BookingSuccess.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BookingSucces from '../components/BookingSuccess/BookingSuccess';
+
+const BookingSucces = () => {
+  return (
+    <div>
+      <BookingSucces/>
+    </div>
+  )
+}
+
+export default BookingSuccess;
---
+++ src/Pages/UpcomingMovie.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UpcomingMovie from '../components/UpcomingMovie/UpcommingMovie';
+
+const UpcomingMovie = () => {
+  return (
+    <div>
+      <UpcomingMovie/>
+    </div>
+  )
+}
+
+export default UpcomingMovie;
