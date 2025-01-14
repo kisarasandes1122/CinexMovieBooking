4cc702888bfb2a66da6b58bd1215c2e7f91c16e0
kisarasandes1122 <kisarasandes22@gmail.com>
2025-01-14 06:43:28 +0530

    Email Function is done

--- src/components/PaymentSection/PaymentMethod.jsx
+++ src/components/PaymentSection/PaymentMethod.jsx
@@ -1,10 +1,49 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import emailjs from '@emailjs/browser';
 
 const PaymentMethod = ({ discountedPrice, movieTitle, selectedDate, selectedTime, selectedSeats, userId, showtimeId, showtimeSeatIds, setPaymentSuccess }) => {
     const [scriptLoaded, setScriptLoaded] = useState(false);
     const [scriptError, setScriptError] = useState(false);
     const navigate = useNavigate();
+    const [userDetails, setUserDetails] = useState(null);
+    const [showtimeDetails, setShowtimeDetails] = useState(null);
+
+
+     const API_BASE_URL = 'http://localhost:27017/api';
+
+    useEffect(() => {
+       const fetchUserDetails = async () => {
+            try{
+              const response = await fetch(`${API_BASE_URL}/auth/${userId}`)
+              if(!response.ok){
+               throw new Error(`HTTP error! status: ${response.status}`)
+             }
+              const data = await response.json();
+              setUserDetails(data);
+             }
+             catch(error){
+                console.error("Failed to fetch user details:", error);
+             }
+         }
+          const fetchShowtimeDetails = async () => {
+            try {
+              const response = await fetch(`${API_BASE_URL}/showtimes/${showtimeId}`);
+              if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+              }
+              const data = await response.json();
+                setShowtimeDetails(data);
+            } catch (error) {
+                console.error("Failed to fetch showtime details:", error);
+            }
+        };
+
+
+         fetchUserDetails();
+         fetchShowtimeDetails();
+
+    }, [userId, showtimeId]);
 
     useEffect(() => {
         const existingScript = document.querySelector('script[src*="paypal"]');
@@ -38,9 +77,9 @@ const PaymentMethod = ({ discountedPrice, movieTitle, selectedDate, selectedTime
                             }]
                         });
                     },
-                     onApprove: async (data, actions) => {
+                    onApprove: async (data, actions) => {
                         try {
-                           const order = await actions.order.capture();
+                            const order = await actions.order.capture();
                             console.log("Payment successful!", order);
 
                             const bookingData = {
@@ -66,6 +105,12 @@ const PaymentMethod = ({ discountedPrice, movieTitle, selectedDate, selectedTime
                             setPaymentSuccess(true);
                             console.log("Booking successful!", newBooking);
 
+                            // send email
+                            if (userDetails && showtimeDetails) {
+                              sendEmail(newBooking, userDetails, showtimeDetails);
+                            }
+
+
                             navigate(`/BookingConfirmation?bookingId=${newBooking._id}`);
                         } catch (error) {
                             console.error("Payment failed or Booking failed:", error);
@@ -90,16 +135,42 @@ const PaymentMethod = ({ discountedPrice, movieTitle, selectedDate, selectedTime
         };
 
         if (!existingScript) {
-              const script = document.createElement('script');
-              script.src = `https://www.paypal.com/sdk/js?client-id=AfIVO7Ti9-VJzpR6sKoU1ympvAdOcNwogZY9zGOYXmQroAAVadJtukjeOK51go2ek_e6esOXAW_cVEvH&currency=USD&intent=capture`;
-              script.async = true;
-              script.onload = handleScriptLoad;
-              script.onerror = handleScriptError;
-              document.body.appendChild(script);
+            const script = document.createElement('script');
+            script.src = `https://www.paypal.com/sdk/js?client-id=AfIVO7Ti9-VJzpR6sKoU1ympvAdOcNwogZY9zGOYXmQroAAVadJtukjeOK51go2ek_e6esOXAW_cVEvH&currency=USD&intent=capture`;
+            script.async = true;
+            script.onload = handleScriptLoad;
+            script.onerror = handleScriptError;
+            document.body.appendChild(script);
         } else {
             handleScriptLoad();
         }
-    }, [discountedPrice, movieTitle, selectedDate, selectedTime, selectedSeats, userId, showtimeId, showtimeSeatIds, setPaymentSuccess, navigate]);
+    }, [discountedPrice, movieTitle, selectedDate, selectedTime, selectedSeats, userId, showtimeId, showtimeSeatIds, setPaymentSuccess, navigate, userDetails, showtimeDetails]);
+
+
+   const sendEmail = async (booking, userDetails, showtimeDetails) => {
+     const templateParams = {
+      ticketNo: booking.ticketNo,
+      movieTitle: movieTitle,
+      TheatreLocation: showtimeDetails?.screenId?.theatreId?.location || "Unknown Location",
+       seatcount: booking.seatCount,
+        format: showtimeDetails?.screenId?.format || "Unknown Format",
+       seats: selectedSeats.join(","),
+       showtime_date: selectedDate,
+        showtime_time: selectedTime,
+        firstName: userDetails.firstName,
+       lastName: userDetails.lastName,
+        mobile: userDetails.mobile,
+       email: userDetails.email
+    };
+      try {
+        const response = await emailjs.send('service_yfr9uzr', 'template_83lg98t', templateParams, '5kn-C6AOOsXF5Hp_k');
+
+        console.log('Email sent successfully', response);
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
+  };
+
 
     return (
         <div className="payment-section">
