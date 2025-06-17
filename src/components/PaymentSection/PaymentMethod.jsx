db7c0111f5d8f90cf0963888476d506e606e20bd
Kisara Sandes <148954008+kisarasandes1122@users.noreply.github.com>
2025-06-17 16:48:26 +0530

    Updated PaymentMethod.jsx

--- src/components/PaymentSection/PaymentMethod.jsx
+++ src/components/PaymentSection/PaymentMethod.jsx
@@ -88,7 +88,7 @@ const PaymentMethod = ({ discountedPrice, movieTitle, selectedDate, selectedTime
                                 showtimeSeatIds: showtimeSeatIds
                             };
 
-                            const response = await fetch('http://localhost:27017/api/bookings/', {
+                            const response = await fetch('https://0735-2402-4000-2300-2930-744c-1b57-deb8-3da0.ngrok-free.app/api/bookings/', {
                                 method: 'POST',
                                 headers: {
                                     'Content-Type': 'application/json',
